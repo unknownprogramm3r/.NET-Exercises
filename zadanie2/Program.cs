cce33d4df961d5a467e5b5dcca465439166f1354
Sławek <skaczmarek88@gmail.com>
2018-03-27 19:39:46 +0200

    zadanie 2

---
+++ zadanie2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var kolo = new Kolo{
+            //     promien = 3
+            // };
+            //koloData.promien = 3;
+
+            Kolo kolo = new Kolo();
+            kolo.promien = 3;
+            kolo.obliczPole();
+            kolo.wypiszPole();
+
+            
+        }
+    }
+}
