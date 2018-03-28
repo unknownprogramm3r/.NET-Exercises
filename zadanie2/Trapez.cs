0ce916cfae36836c6dd16c69cbfde1737293cdda
Sławek <skaczmarek88@gmail.com>
2018-03-28 13:37:55 +0200

    algorytm dla trapezu

--- zadanie2/Trapez.cs
+++ zadanie2/Trapez.cs
@@ -1,28 +1,34 @@
 using System;
 
-public class Prostokat : Figura {
+public class Trapez : Figura {
 
     public double podstawaA {get; set;}
     public double podstawaB {get; set;} 
-    public double wysokosc {get; set;} 
+    public double wysokosc {get; set;}
+    public double bokA {get; set;}
+    public double bokB {get; set;}  
+
     
     // Przeciążenie metod z klasy abstrakcyjnej Figura
 
     override public double obliczPole(){
-        double pole = ((podstawaA + podstawaB) * wysokosc) / 2;
-        return pole;
+        if(podstawaA > 0 && podstawaB > 0 && wysokosc > 0){
+            double pole = ((podstawaA + podstawaB) * wysokosc) / 2;
+            return pole;
+        }    
+        else if ()
     }
 
     override public double obliczObwod(){
-        double obwod = 2 * bokA + 2 * bokB;
+        double obwod = podstawaA + podstawaB + bokA + bokB;
         return obwod;
     }
 
+    public double obliczBoki(){}
+
 
     // konstruktor :
-    public Prostokat(){
-
-    }
+    public Trapez(){}
 
 
 }
